390ca7192b1e59da66ada66bbc336a381cd4fc5f
Paul <Paul_L_Preston@hotmail.com>
2019-11-24 19:28:17 +1000

    Cleaned up a lot of the code because it was missing @Override and was using deprecated spec methods. Also a bug in the aura spec but I don't think that's used.

--- src/main/java/com/m1zark/customspec/specs/TextureSpec.java
+++ src/main/java/com/m1zark/customspec/specs/TextureSpec.java
@@ -11,63 +11,55 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TextureSpec extends SpecValue<String> implements ISpecType {
-    public TextureSpec(String value) {
-        super("texture", value);
-    }
-
-    public List<String> getKeys() {
-        return Arrays.asList("texture");
-    }
-
-    @Override public Class<? extends SpecValue<?>> getSpecClass() {
-        return this.getClass();
-    }
-
-    @Override public Class<String> getValueClass() {
-        return String.class;
-    }
-
-    @Override public TextureSpec parse(String arg) {
-        return new TextureSpec(arg);
-    }
-
-    @Override public String toParameterForm(SpecValue<?> value) {
-        return value.key + ":" + value.value.toString();
-    }
-
-    @Override public SpecValue<String> clone() {
-        return new TextureSpec(this.value);
-    }
-
-    @Override public TextureSpec readFromNBT(NBTTagCompound nbt) {
-        return parse(nbt.getString(NbtKeys.CUSTOM_TEXTURE));
-    }
-
-    @Override public void writeToNBT(NBTTagCompound nbt, SpecValue<?> value) {
-        nbt.setString(NbtKeys.CUSTOM_TEXTURE, value.value.toString());
-    }
-
-    @Override public boolean matches(EntityPixelmon pixelmon) {
-        return this.matches(pixelmon.getPokemonData());
-    }
-
-    @Override public boolean matches(Pokemon pokemon) {
-        return pokemon.getCustomTexture().equalsIgnoreCase(this.value);
-    }
-
-    @Override public boolean matches(NBTTagCompound nbt) {
-        return nbt.getString(NbtKeys.CUSTOM_TEXTURE).equalsIgnoreCase(this.value);
-    }
-
-    @Override public void apply(EntityPixelmon pixelmon) {
-        this.apply(pixelmon.getPokemonData());
-    }
-
-    @Override public void apply(Pokemon pokemon) {
-        pokemon.setCustomTexture(this.value);
-    }
-
-    @Override public void apply(NBTTagCompound nbt) {
-        nbt.setString(NbtKeys.CUSTOM_TEXTURE, this.value);
-    }
+	public TextureSpec(String value) {
+		super("texture", value);
+	}
+	
+	@Override public List<String> getKeys() {
+		return Arrays.asList("texture");
+	}
+	
+	@Override public Class<? extends SpecValue<?>> getSpecClass() {
+		return this.getClass();
+	}
+	
+	@Override public Class<String> getValueClass() {
+		return String.class;
+	}
+	
+	@Override public TextureSpec parse(String arg) {
+		return new TextureSpec(arg);
+	}
+	
+	@Override public String toParameterForm(SpecValue<?> value) {
+		return value.key + ":" + value.value.toString();
+	}
+	
+	@Override public SpecValue<String> clone() {
+		return new TextureSpec(this.value);
+	}
+	
+	@Override public TextureSpec readFromNBT(NBTTagCompound nbt) {
+		return parse(nbt.getString(NbtKeys.CUSTOM_TEXTURE));
+	}
+	
+	@Override public void writeToNBT(NBTTagCompound nbt, SpecValue<?> value) {
+		nbt.setString(NbtKeys.CUSTOM_TEXTURE, value.value.toString());
+	}
+	
+	@Override public boolean matches(EntityPixelmon pixelmon) {
+		return this.matches(pixelmon.getPokemonData());
+	}
+	
+	@Override public boolean matches(Pokemon pokemon) {
+		return pokemon.getCustomTexture().equalsIgnoreCase(this.value);
+	}
+	
+	@Override public void apply(EntityPixelmon pixelmon) {
+		this.apply(pixelmon.getPokemonData());
+	}
+	
+	@Override public void apply(Pokemon pokemon) {
+		pokemon.setCustomTexture(this.value);
+	}
 }
